f613a0e772e87b8b0f368b34787826ac16c4c2c2
Steven Martin <stevenisekimartin@gmail.com>
2018-10-19 00:38:56 +1100

    Add Complete, Incomplete screens

---
+++ src/screens/Complete.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+import { ContainerView } from '../styles/typography'
+import { GoalList } from '../components/GoalList'
+import { addGoal, removeGoal, reopenGoal, completeGoal } from '../actions/goals'
+
+class CompleteScreen extends Component {
+  render() {
+    return (
+      <ContainerView>
+        <GoalList {...this.props} />
+      </ContainerView>
+    )
+  }
+}
+
+const mapDispatchToProps = dispatch => {
+  return {
+    addGoal: goal => dispatch(addGoal(goal)),
+    removeGoal: goal => dispatch(removeGoal(goal)),
+    reopenGoal: goal => dispatch(reopenGoal(goal)),
+    completeGoal: goal => dispatch(completeGoal(goal))
+  }
+}
+
+const mapStateToProps = state => ({
+  goals: state.Goals.goals
+})
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(CompleteScreen)
