fa4ac5dd586be1d4ea07c59163c8644f34272dcf
Steven Martin <stevenisekimartin@gmail.com>
2018-10-19 03:22:57 +1100

    Remove custom font

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React, { Component } from 'react'
-import { Font } from 'expo'
 import { StatusBar } from 'react-native'
 import { Provider } from 'react-redux'
 import { ThemeProvider } from 'styled-components'
 import { Root } from './styles/typography'
 
-
 import store from './store'
 import Navigator from './Navigator'
 import { colors } from './styles/colors'
-import { AppLoader } from './components/AppLoader'
 
 class RootContainer extends Component {
   render() {
@@ -29,20 +26,10 @@ class RootContainer extends Component {
 }
 
 class App extends Component {
-  state = {
-    fontsLoaded: false
-  }
-
-  async componentWillMount() {
-    /* await Font.loadAsync({}) */
-    this.setState({ fontsLoaded: true })
-  }
-
   render() {
-    const { fontsLoaded } = this.state
     return (
       <Provider store={store}>
-        {fontsLoaded ? <RootContainer /> : <AppLoader />}
+        <RootContainer />
       </Provider>
     )
   }
